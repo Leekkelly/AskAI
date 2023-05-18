3031ffb67db7f0190eebf0586f87400a7520f520
Leekkelly <kellylee.au@gmail.com>
2023-05-18 15:32:12 +1000

    update the server url

--- client/script.js
+++ client/script.js
@@ -75,7 +75,7 @@ const handleSubmit = async (e) => {
 
     loader(messageDiv);
 
-    const response = await fetch('http://localhost:5055', {
+    const response = await fetch('https://askai-8k21.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
